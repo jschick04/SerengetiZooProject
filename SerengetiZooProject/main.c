9d182cc3e2f5d19b815b3b5f485a65a833aa8d07
Joseph Schick <jschick@microsoft.com>
2019-12-16 15:50:22 +0000

    Merged PR 1: Created and initialized visitor and animal lists and ZooAnimal struct
    Related work items: #42

--- SerengetiZooProject/main.c
+++ SerengetiZooProject/main.c
@@ -1,6 +1,52 @@
 #include <Windows.h>
 #include <tchar.h>
 
+enum AnimalType { Antelopes, Giraffes, Hyaena, Hippos, Monkeys, Mongoose, Tigers, WildeBeast, Zebras };
+
+typedef struct NodeEntry {
+    struct NodeEntry* Flink;
+    struct NodeEntry* Blink;
+} NodeEntry;
+
+typedef struct ZooAnimal {
+    enum AnimalType AnimalType;
+    LPTSTR UniqueName;
+    LPTSTR CageName;
+    DWORD HealthLevel;
+    BOOL HealthLevelChange;
+    DWORD InteractiveLevel;
+    BOOL InteractivityPrompted;
+} ZooAnimal;
+
+CRITICAL_SECTION AnimalListCrit;
+NodeEntry* animalListHead = 0;
+NodeEntry* visitorListHead = 0;
+
+BOOL InitializeListHead() {
+    animalListHead = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(NodeEntry));
+    visitorListHead = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(NodeEntry));
+
+    if (animalListHead == 0 || visitorListHead == 0) {
+        return FALSE;
+    }
+
+    animalListHead->Flink = animalListHead->Blink = animalListHead;
+    visitorListHead->Flink = visitorListHead->Blink = visitorListHead;
+
+    return TRUE;
+}
+
 int _tmain() {
-    // TODO: implement main loop
+    if (InitializeListHead() == FALSE) {
+        return -1;
+    }
+
+    if (InitializeCriticalSectionAndSpinCount(&AnimalListCrit, 4000) == FALSE) {
+        return -1;
+    }
+
+    // TODO: Implement Main Loop
+
+    HeapFree(GetProcessHeap(), 0, &animalListHead);
+    HeapFree(GetProcessHeap(), 0, &visitorListHead);
 }
