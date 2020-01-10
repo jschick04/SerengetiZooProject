4fe341b3b4a2dc827ae92c6db47920d8039bb1a3
Joseph Schick <jschick@microsoft.com>
2020-01-10 15:23:22 -0600

    Reverted list to LPCTSTR array for now

--- SerengetiZooProject/Helpers.cpp
+++ SerengetiZooProject/Helpers.cpp
@@ -1,4 +1,5 @@
 #include "Helpers.h"
+#include <tchar.h>
 
 void Helpers::AddRandomName(LPCTSTR name) {
     EnterCriticalSection(&NameListCrit);
@@ -28,3 +29,59 @@ LPCTSTR Helpers::GetRandomName() {
 
     return selectedName;
 }
+
+LPCTSTR Helpers::m_uniqueNames[] = {
+    _T("Julien"),
+    _T("Melman"),
+    _T("Julien"),
+    _T("Melman"),
+    _T("Maurice"),
+    _T("Gloria"),
+    _T("Mason"),
+    _T("Ron"),
+    _T("Deveroux"),
+    _T("Felicio"),
+    _T("Gary"),
+    _T("Jorge"),
+    _T("Joseph"),
+    _T("Travis"),
+    _T("Ryan"),
+    _T("Tony"),
+    _T("Leonardo"),
+    _T("Barry"),
+    _T("David"),
+    _T("Bruce"),
+    _T("Cliff"),
+    _T("Jack"),
+    _T("Emma"),
+    _T("Taylor"),
+    _T("Alex"),
+    _T("Sophia"),
+    _T("James"),
+    _T("Diego"),
+    _T("Samuel"),
+    _T("Maria"),
+    _T("Richard"),
+    _T("Logan"),
+    _T("Brianna"),
+    _T("Martin"),
+    _T("Jessica"),
+    _T("Patricia"),
+    _T("Linda"),
+    _T("Caroline"),
+    _T("Liam"),
+    _T("Oliver"),
+    _T("Ben"),
+    _T("Dorothy"),
+    _T("Mabel"),
+    _T("Ariel"),
+    _T("Louis"),
+    _T("Nathan"),
+    _T("Nicole"),
+    _T("Adam"),
+    _T("Felix"),
+    _T("Jon"),
+    _T("Marco"),
+    _T("Harry"),
+    _T("Anna")
+};
